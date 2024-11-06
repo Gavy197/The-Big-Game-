0074258ccb40b470cdb81c8d32ec941b85b881de
Zane <zagrismore@gmail.com>
2024-11-05 20:44:41 -0500

    Adds basic movement

--- main_player.gd
+++ main_player.gd
@@ -1,25 +1,44 @@
 extends CharacterBody2D
 
-
+#Constants
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+
+#Varibales
+@export var health: int = 100
+
+#Onreadys
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	# Handles walking left/right.
+	var hDirection := Input.get_axis("walkLeft", "walkRight")
+	if hDirection:
+		#Moves the player left or right based on the button you click
+		velocity.x = hDirection * SPEED
 	else:
+		#Stops the player when you aren't clicking anything
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	# Handles walking up/down.
+	var vDirection := Input.get_axis("walkUp","walkDown")
+	if vDirection:
+		#Moves the player up or dwon based on the button you click
+		velocity.y = vDirection*SPEED
+	else:
+		#Stops the player when you aren't clicking anything
+		velocity.y= move_toward(velocity.y,0,SPEED)
+		
+	#Handles movement animations
+	if hDirection!=0 or vDirection !=0:
+		#Plays Walk if moving
+		animated_sprite_2d.play("Walk")
+	else:
+		#Plays idle when not moving
+		animated_sprite_2d.play("Idle")
+	#Flips spirte when going left
+	if hDirection<0:
+		animated_sprite_2d.flip_h=true
+	elif hDirection>0:
+		animated_sprite_2d.flip_h=false
 	move_and_slide()
