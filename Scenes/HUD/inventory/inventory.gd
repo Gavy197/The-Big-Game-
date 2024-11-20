a441ed969fa43057814d8c4655bf8aebdfa55e00
Gavy197 <ValakVox@gmail.com>
2024-11-20 13:16:31 -0500

    finish collision for resources

--- Scenes/HUD/inventory/inventory.gd
+++ Scenes/HUD/inventory/inventory.gd
@@ -1,8 +1,14 @@
 extends Resource
-
+#class
 class_name Inventory
+#signal
+signal updated
 
 @export var items: Array[InventoryItem]
 
 func insert(item: InventoryItem):
-	pass
+	for i in range(items.size()):
+		if !items[i]:
+			items[i] = item
+			break
+	updated.emit()
--- Scripts/gui/collectable.gd
+++ Scripts/gui/collectable.gd
@@ -1,4 +1,5 @@
 extends Area2D
+
 class_name collectable
 
 @export var itemRes: InventoryItem
