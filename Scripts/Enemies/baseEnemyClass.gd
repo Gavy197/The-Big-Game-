28ab92ef83d802cea85895f90f0f17c8cda48ccf
Zane <zagrismore@gmail.com>
2024-11-25 13:17:18 -0500

    Added Goblin

--- Scripts/Enemies/baseEnemyClass.gd
+++ Scripts/Enemies/baseEnemyClass.gd
@@ -2,13 +2,33 @@ extends CharacterBody2D
 
 class_name Enemy
 
-const SPEED = 300.0
-
+#Variables
+const SPEED = 100.0
+var startingDir:Vector2=Vector2(0,0)
+#Onreadys
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D 
 
 func _ready():
-	pass
+	var randStart=randi_range(1,4)
+	print(randStart)
+	#Picks a random direction to start walking in, until it finds a player, or a wall
+	if randStart ==1:
+		startingDir.x=1
+	elif randStart ==2:
+		startingDir.x=-1
+	elif randStart ==3:
+		startingDir.y=-1
+	elif randStart ==4:
+		startingDir.y=-1
+	velocity=startingDir*Vector2(SPEED,SPEED)
 
 func _physics_process(delta: float) -> void:
-	
+	if velocity.x>0:
+		animated_sprite_2d.flip_h=true
+	else:
+		animated_sprite_2d.flip_h=false
+
+
+
 
 	move_and_slide()
