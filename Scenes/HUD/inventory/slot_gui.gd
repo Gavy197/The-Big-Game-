218c232315acbdf5da421ee36243ba0be82344d8
Gavy197 <ValakVox@gmail.com>
2024-12-10 13:17:22 -0500

    fix

--- Scenes/HUD/inventory/slot_gui.gd
+++ Scenes/HUD/inventory/slot_gui.gd
@@ -6,11 +6,14 @@ extends Button
 @onready var itemSprite: Sprite2D = $CenterContainer/Panel/item
 @onready var amountLabel: Label = $CenterContainer/Panel/Label
 @onready var main_player: CharacterBody2D = $"../../../../../Player"
-@export var inventory: Inventory
+@onready var emptySlot=preload("res://Scenes/HUD/inventory/slot_gui.tscn")
+@onready var inventory: Inventory = preload("res://Scenes/HUD/PlayerInventory.tres")
+var selfSlot:InventorySlot
 
 
 func update(slot: InventorySlot):
-	#if the item is null
+	selfSlot=slot
+	#if the item is nulls
 	if !slot.item:
 		#replace background sprite to 0
 		backgroundSprite.frame = 0
@@ -24,32 +27,41 @@ func update(slot: InventorySlot):
 		amountLabel.visible = true
 		amountLabel.text = str(slot.amount)
 		
-func remove(inventory: Inventory,itemRes):
-	inventory.remove(InventoryItem)
-	queue_free()
+func remove():
+	selfSlot.amount-=1
+	update(selfSlot)
+	if selfSlot.amount<=0:
+		queue_free()
+		var instance = emptySlot.instantiate()
+		add_sibling(instance)
+		instance.update(inventory.get_parent().get_children()[self.get_index()])
 #coin  = <CompressedTexture2D#-9223372000397884039>
 #consume items function
 func _on_pressed() -> void:
-	if itemSprite.texture == ResourceLoader.load("res://Assets/Pickups/GoldCoin.png"):
-		if itemSprite.visible == true:
-			print(main_player.currentHealth)
-			#remove(inventory,InventoryItem)
-#-----------------------------------------------------------------------
-#beef (food) + 5 hp
-	if itemSprite.texture == ResourceLoader.load("res://Assets/Pickups/Beaf.png"):
-		if itemSprite.visible == true and main_player.currentHealth <= main_player.maxHealth -5:
-			main_player.currentHealth += 5
-			print(main_player.currentHealth)
-#------------------------------------------------------------------------
-#calamri (food) + 10 hp
-	if itemSprite.texture == ResourceLoader.load("res://Assets/Pickups/Calamari.png"):
-		if itemSprite.visible == true and main_player.maxHealth <= 90:
-			main_player.currentHealth += 10
-			print(main_player.currentHealth)
-#-----------------------------------------------------------------------
-#medpack + 25 hp
-	if itemSprite.texture == ResourceLoader.load("res://Assets/Pickups/Medipack.png"):
-		if itemSprite.visible == true and main_player.currentHealth <= 75:
-			main_player.maxHealth += 25
-			print(main_player.maxHealth)
-		
+	if selfSlot.amount>0:
+		if itemSprite.texture == ResourceLoader.load("res://Assets/Pickups/GoldCoin.png"):
+			if itemSprite.visible == true:
+				print(main_player.currentHealth)
+				remove()
+	#-----------------------------------------------------------------------
+	#beef (food) + 5 hp
+		if itemSprite.texture == ResourceLoader.load("res://Assets/Pickups/Beaf.png"):
+			if itemSprite.visible == true and main_player.currentHealth <= main_player.maxHealth -5:
+				main_player.currentHealth += 5
+				print(main_player.currentHealth)
+				remove()
+	#------------------------------------------------------------------------
+	#calamri (food) + 10 hp
+		if itemSprite.texture == ResourceLoader.load("res://Assets/Pickups/Calamari.png"):
+			if itemSprite.visible == true and main_player.currentHealth <= main_player.maxHealth - 10:
+				main_player.currentHealth += 10
+				print(main_player.currentHealth)
+				remove()
+	#-----------------------------------------------------------------------
+	#medpack + 25 hp
+		if itemSprite.texture == ResourceLoader.load("res://Assets/Pickups/Medipack.png"):
+			if itemSprite.visible == true:
+				main_player.maxHealth += 25
+				print(main_player.maxHealth)
+				remove()
+			
