41a076e785e0552708ddb2e41376cda279ab04e8
Gavy197 <ValakVox@gmail.com>
2024-12-13 11:14:56 -0500

    Game paused text

--- Scripts/gui/canvas_layer.gd
+++ Scripts/gui/canvas_layer.gd
@@ -16,7 +16,9 @@ func _input(event):
 #unpause when closed
 func _on_inventory_gui_closed() -> void:
 	get_tree().paused = false
+	get_child(2).visible = false
 
 #pause when open
 func _on_inventory_gui_opened() -> void:
 	get_tree().paused = true
+	get_child(2).visible = true
