9bcf469b120ed4659684db0c53daeab69b0da8a8
danfoley0 <danieljfoley@icloud.com>
2024-12-13 10:52:20 -0500

    bug fix

--- Scenes/Levels/portal.gd
+++ Scenes/Levels/portal.gd
@@ -9,19 +9,9 @@ func _ready():
 	exit_position=global_position+exit_offset
 	pass # Replace with function body.
 func _on_body_entered(body: Node2D) -> void:
-	if isexit:
+	if isexit==true:
 		if body.name=="Player":
 			get_tree().change_scene_to_file("res://Scenes/Levels/dungeon.tscn")
-		isexit=false
 	else:
 		if body.name=="Player":
 			get_tree().change_scene_to_file("res://Scenes/Levels/overworld.tscn")
-		isexit=false
-	if body.name==("Player"):
-		pass
-		#if isexit==true:
-			#body.exit_position=exit_position
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
